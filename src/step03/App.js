c7ac40e4ee57a4606747348fd5f2adbc5241b5b6
ssssRssssungjune <ssungjune0610@gmail.com>
2024-07-29 15:01:47 +0900

    step06 내비게이션 스타일

--- src/step03/App.js
+++ src/step03/App.js
@@ -1,44 +1,40 @@
-//1.로딩페이지1
+// 1.로딩페이지
 import './App.css';
-import {useState,useEffect} from'react';
+import {useState, useEffect} from 'react';
+
 
 function App() {
   //상태변수를 설정
   const [isLoading, setIsLoading] = useState(true);
   const [loadCounter, setLoadCounter] = useState(0);
-  
-
-
-
-
   useEffect(
-    ()=>{
-
+    ()=>{      
       function IncreaseCounter(){
         let _counter = loadCounter;
         _counter = _counter + 1;
-        if(_counter > 10){
+        // 카운터값이 10이상이면 Loading종료, 타이머 종료
+        if(_counter >10){
           setIsLoading(false);
           clearInterval(loadTimer);
-          
         }
         setLoadCounter(
           _counter
         );
       }
-
+      //타이머로 loadcounter 값을 1씩 증가
       const loadTimer = setInterval(
-        IncreaseCounter,1000
+        IncreaseCounter,1000             
       );
-      return()=>{ clearInterval(loadTimer); }
+      return ()=>{ clearInterval(loadTimer); }
+
     },[loadCounter]
-  )
+
+  );
 
   return (
-    <div>
-      {isLoading? `Loading...${loadCounter}`:'Loaded'}
+    <div >
+      {isLoading? `Loading... ${loadCounter}`: 'Loaded'}
     </div>
-    
   );
 }
 
---
+++ src/step06/App.js
@@ -0,0 +1,23 @@
+// 3.Router적용
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Detail } from './pages/Detail';
+import { Navigation } from './components/Navigation';
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Navigation/>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/detail" element={<Detail/>}/>
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
