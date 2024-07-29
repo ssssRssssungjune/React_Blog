9baadb611c327d5b6755b4334865583fdb0a35b9
ssssRssssungjune <ssungjune0610@gmail.com>
2024-07-29 17:02:42 +0900

    리액트로 영화리뷰 사이트

--- src/step07/components/Movie.jsx
+++ src/step07/components/Movie.jsx
@@ -5,22 +5,24 @@ export function Movie({id, year, title, summary, poster, genres}){
 
   return (
   <div className='movie'>
-    <img src={poster} alt={title}/>
-    <div className='movie_data'>
-      <h3 className='movie_title'>{title}</h3>
-      <h5 className='movie_year'>{year}</h5>
-      <p className='movie_summary'>{summary.slice(0,180)}...</p>
-      <ul className="movie_genres">        
-      {
-        genres.map(
-          (genre,index) =>(
-            <li key={index} className='movie_genre'>{genre}</li>
+    <Link to={`/detail?id=${id}`}>
+      <img src={poster} alt={title}/>
+      <div className='movie_data'>
+        <h3 className='movie_title'>{title}</h3>
+        <h5 className='movie_year'>{year}</h5>
+        <p className='movie_summary'>{summary.slice(0,180)}...</p>
+        <ul className="movie_genres">        
+        {
+          genres.map(
+            (genre,index) =>(
+              <li key={index} className='movie_genre'>{genre}</li>
+            )
           )
-        )
-      }
-      </ul>
+        }
+        </ul>
 
-    </div>
+      </div>
+    </Link>
   </div>
   );
 }
